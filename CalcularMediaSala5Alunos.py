51e628d2b21a5acafe87637612db33712ca00fca
Fitz <142354039+fitzinn@users.noreply.github.com>
2024-03-04 11:26:20 -0300

    Create CalcularMediaSala5Alunos.py
    Program to calculate average of a class of 5 students

---
+++ CalcularMediaSala5Alunos.py
@@ -0,0 +1,11 @@
+n1=float(input("Digite a nota do aluno 1: "))
+n2=float(input("Digite a nota do aluno 2: "))
+n3=float(input("Digite a nota do aluno 3: "))
+n4=float(input("Digite a nota do aluno 4: "))
+n5=float(input("Digite a nota do aluno 5: "))
+
+#calculo
+m= (n1 + n2 + n3 + n4 + n5) / 5
+
+#imprimir
+print("Media dos 5 alunos: ", format(m, "3.2"))
