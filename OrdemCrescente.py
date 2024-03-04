49e966417aeb5b8ab6339db003ae4a1032ca9c9e
Fitz <142354039+fitzinn@users.noreply.github.com>
2024-03-04 14:11:38 -0300

    Create OrdemCrescente.py
    Program to check if the numbers are in crescent order

---
+++ OrdemCrescente.py
@@ -0,0 +1,8 @@
+n1=float(input("Digite o primeiro numero: "))
+n2=float(input("Digite o primeiro numero: "))
+n3=float(input("Digite o primeiro numero: "))
+
+if(n1<n2 and n2<n3):
+    print("Em ordem crescente.")
+else:
+    print("Fora de ordem.")
